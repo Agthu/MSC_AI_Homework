416fb4c6a9ca3a2a650e19a3a9223cedc20ac9a0
Qie-Dan <13799658498@163.com>
2020-04-16 15:33:06 +0800

    我不想写commit message

--- src/homework1/LinearRegression.py
+++ src/homework1/LinearRegression.py
@@ -2,5 +2,48 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# # 等差数列
+# ar = np.linspace(0, 1, 11)
+# print(ar)
+# # 等比数列
+# a = np.logspace(1, 2, 2)
+# print(a)
+# # 全0
+# print(np.zeros(10, np.int))
+# # 全1
+# print(np.ones((9, 2), np.int))
+# # 空
+# print(np.empty((2, 3)))
 
+
+# def func(i):
+#     return i*2
+#
+#
+# # 第一个参数为计算每个数组元素的函数，第二个参数为数组的大小(shape)
+# a = np.fromfunction(func, (10, ))
+# print(a)
+
+
+# a = np.array([10, 9, 8, 7, 6, 5, 4])
+# print(a)
+# b = a[[1, 1, 6, 2]]
+# print(b)
+
+
+# # 结构数组
+# persontype = np.dtype({
+#     'names': ['name', 'age', 'weight'],
+#     'formats': ['S32', 'i', 'f']
+# })
+#
+# a = np.array([("Zhang", 32, 75.5), ("Wang", 24, 65.2)], dtype=persontype)
+# print(a)
+
+
+# # 内置操作函数
+# print(np.log(np.e))
+# # >>>1.0
+
+# --------------------------------------以上全是笔记--------------------------------------------
 
